5576573d3653604df5a99fa9d82acc6f7bfc379e
alvinljt <alvinljt@gmail.com>
2020-11-03 14:38:04 +0800

    refactored code for base section

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
-// Global variables ----------------------------------------------
-// Get and store the 3rd value entered in the terminal as a string
-const hexCode = process.argv[2];
-/* Note: need to use '<value>' for hexcode. Eg. use '#ffffff'
-instead of #ffffff. This is because the '#' sign is used to tell
-the shell that what follows is just a comment, not an argument.
-Also, process.argv always returns a string array.
-*/
+// Global variables
+// ----------------------------------------------
 
-// Helper functions ----------------------------------------------
+// Helper functions
+// ----------------------------------------------
 // Convert hex to rgb and returns an object with r,g,b as keys.
 function convertHexToRgb(hex) {
   const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
@@ -18,12 +13,29 @@ function convertHexToRgb(hex) {
   } : null;
 }
 
-// Program logic ------------------------------------------------
+// Get the output message to display RGB that was converted from hex code
+function getOutputForRGBConvertedFromHexCode(object) {
+  return `rgb(${object.r},${object.g},${object.b})`;
+}
+
+// Convert rgb to hex and returns an object
+
+// Program logic
+// ------------------------------------------------
+// Store the 3rd value entered in the terminal as a string
+const hexCode = process.argv[2];
+/* Note: need to use '<value>' for hexcode. Eg. use '#ffffff'
+instead of #ffffff. This is because the '#' sign is used to tell
+the shell that what follows is just a comment, not an argument.
+Also, process.argv always returns a string array.
+*/
+
 // Convert hex code to rgb and store the r,g,b as keys and values in an object
 const RGBObjectConvertedFromHexCode = convertHexToRgb(hexCode);
 
-// The output message to display RGB that was converted from hex code
-const outputForRGBConvertedFromHexCode = `rgb(${RGBObjectConvertedFromHexCode.r},${RGBObjectConvertedFromHexCode.g},${RGBObjectConvertedFromHexCode.b})`;
+// Get the output message to display RGB that was converted from hex code
+// eslint-disable-next-line max-len
+const outputForRGBConvertedFromHexCode = getOutputForRGBConvertedFromHexCode(RGBObjectConvertedFromHexCode);
 
 // Console.log to display the output message in the terminal
 console.log(outputForRGBConvertedFromHexCode);
